f13cc1c81da80d7f37220cf73da3c5b80eb0649c
Nikita Syulin <nikita_sulin@idpowers.com>
2022-04-23 15:58:26 +0700

    change gui

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-import qrcode
+# import qrcode
+#
+#
+# data = """
+# https://keep.google.com/u/0/#NOTE/1GCkc5WjLg9C3MfHZQFamTUNNE5MVJ-BldERVz2zS2-jQMDtRs8YriLFcBnK96f1Fwus
+# """
+#
+# img = qrcode.make(data)
+# type(img)  # qrcode.image.pil.PilImage
+# img.save("some_file.png")
 
 
-with open('index.html', 'r') as f:
-    text = f.read()
-data = """
+import tkinter
+import customtkinter
 
-"""
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
-img = qrcode.make(text)
-type(img)  # qrcode.image.pil.PilImage
-img.save("some_file.png")
+root_tk = customtkinter.CTk()  # create CTk window like you do with the Tk window
+root_tk.geometry("400x240")
+
+
+def button_function():
+    print("button pressed")
+
+
+# Use CTkButton instead of tkinter Button
+button = customtkinter.CTkButton(master=root_tk, text="CTkButton", command=button_function)
+button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+root_tk.mainloop()
